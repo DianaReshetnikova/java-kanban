51d98fe7221776c6b79104f74eefaa284e2657b3
Diana Reshetnikova <i@dharchenko.ru>
2025-04-13 22:58:25 +0300

    Перенести класс Node в файл InMemoryHistoryManager.java

--- src/ru/yandex/practicum/service/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/service/InMemoryHistoryManager.java
@@ -93,3 +93,16 @@ public class InMemoryHistoryManager implements HistoryManager {
         tasksHistoryMap.remove(node.task.getId());
     }
 }
+
+//Класс Node описывает упрощенный узел двусвязного списка LinkedList
+class Node {
+    Task task;
+    Node next;
+    Node prev;
+
+    public Node(Task task, Node next, Node prev) {
+        this.task = task;
+        this.next = next;
+        this.prev = prev;
+    }
+}
